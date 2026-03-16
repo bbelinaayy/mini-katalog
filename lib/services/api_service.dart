663bff9c67bb84ac7aed978b03f997d18a9526dc
bbelinaayy <belinaymert721@gmail.com>
2026-03-16 22:51:01 +0300

    Initial commit

---
+++ lib/services/api_service.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:katalog/model/product_model.dart';
+
+class ApiService {
+  Future<ProductModel> fetchProducts() async {
+    final response = await http.get(
+      Uri.parse('https://www.wantapi.com/products.php'),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      return ProductModel.fromJson(data);
+    } else {
+      throw Exception('Failed to load products');
+    }
+  }
+}
